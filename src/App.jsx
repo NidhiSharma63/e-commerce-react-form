5683c749756b68fc59149cf69240dd6c21aee08b
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-08-05 17:07:05 +0530

    merchant png added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Form from './components/Form';
 import AccountConfirmation from './components/Account-confirmation';
+import mainImg from './images/main.webp';
 
 const App = () => {
   const [isFormSubmitted, setIsFormSubmitted] = React.useState(false);
 
   return (
     <div className='main-wrapper'>
+      <div className="images">
+        <img src={mainImg} alt="img" />
+      </div>
       {isFormSubmitted ? <AccountConfirmation /> : <Form setIsFormSubmitted={setIsFormSubmitted} />}
     </div>
   )
